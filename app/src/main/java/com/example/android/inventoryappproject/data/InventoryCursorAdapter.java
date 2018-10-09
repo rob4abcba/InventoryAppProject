ef3a9852dfc7ae1a0de3fc53b03f0e8658aa5474
Erick <eruelas@angelo.edu>
2018-10-08 22:44:42 -0500

    Changes done on Activitys

---
+++ app/src/main/java/com/example/android/inventoryappproject/data/InventoryCursorAdapter.java
@@ -0,0 +1,68 @@
+package com.example.android.inventoryappproject.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.inventoryappproject.R;
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link InventoryCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+
+    public InventoryCursorAdapter (Context context, Cursor c) {super(context,c, 0 /*flaga */);}
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+    /**
+     *
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+
+
+        int nameColumnIndex = cursor.getColumnIndex(InventoryContract.inventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME);
+        int breedColumnIndex = cursor.getColumnIndex(InventoryContract.inventoryEntry.COLUMN_INVENTORY_PRICE);
+
+
+        String petName = cursor.getString(nameColumnIndex);
+
+
+
+        // Update the TextViews with the attributes for the current pet
+        nameTextView.setText(petName);
+
+    }
+
+}
