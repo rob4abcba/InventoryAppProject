a791af2dda1683f7b916301979d7a9e1218b237d
Erick <eruelas@angelo.edu>
2018-10-31 21:17:01 -0500

    modify the   catalog activity and the xml

--- app/src/main/java/com/example/android/inventoryappproject/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryappproject/CatalogActivity.java
@@ -9,7 +9,10 @@ import android.os.Bundle;
 import android.provider.BaseColumns;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.NavUtils;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -22,117 +25,152 @@ import android.widget.Toast;
 import com.example.android.inventoryappproject.data.InventoryContract;
 import com.example.android.inventoryappproject.data.InventoryCursorAdapter;
 
-public class CatalogActivity  extends AppCompatActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
 
-        private static final int  EXISTING_INVENTORY_LOADER = 0;
-        InventoryCursorAdapter mCursorAdapter;
+        public class CatalogActivity extends AppCompatActivity implements
+                LoaderManager.LoaderCallbacks<Cursor> {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.catalog_activity);
 
-        // Setup FAB to open EditorActivity
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+            private static final int EXISTING_INVENTORY_LOADER = 0;
+            InventoryCursorAdapter mCursorAdapter;
+
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
-                startActivity(intent);
-            }
-        });
-        // Find the ListView which will be populated with the pet data
-        ListView inventoryListView = (ListView) findViewById(R.id.list);
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.catalog_activity);
 
-        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
-        View emptyView = findViewById(R.id.empty_space);
-        inventoryListView.setEmptyView(emptyView);
+                // Setup FAB to open EditorActivity
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                // Find the ListView which will be populated with the pet data
+                ListView inventoryListView = (ListView) findViewById(R.id.list);
 
-        mCursorAdapter = new InventoryCursorAdapter(this, null);
-        inventoryListView.setAdapter(mCursorAdapter);
+                // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+                View emptyView = findViewById(R.id.empty_space);
+                inventoryListView.setEmptyView(emptyView);
 
-    }
-     inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                mCursorAdapter = new InventoryCursorAdapter(this, null);
+                inventoryListView.setAdapter(mCursorAdapter);
 
-            Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
 
-            Uri mCurrentInventoryUri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, id);
+                inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
 
-            startActivity(intent);
-        });
+                        Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
 
-        getLoaderManager().initLoader( EXISTING_INVENTORY_LOADER, null, this);
-    }
-    @Override
-    public boolean onCreateOptionsMenu (Menu menu){
-        getMenuInflater().inflate(R.menu.menu_catalog, menu);
-        return true;
-    }
+                        Uri mCurrentInventoryUri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, id);
 
-    @Override
-    public boolean onOptionsItemSelected (MenuItem item){
+                        startActivity(intent);
+                    }
+                });
 
-        switch (item.getItemId()) {
-
-            case R.id.action_insert_dummy_data:
-                insertdata();
-                return true;
-
-            case R.id.action_delete_all_entries:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void insertdata() {
-        ContentValues values = new ContentValues();
-        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME, "Games");
-        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE, 50);
-        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY, 20);
-        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_NAME, "PetsSmart");
-        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER, "205-605-4040");
-        Uri uri = getContentResolver().insert(InventoryContract.InventoryEntry.CONTENT_URI, values);
-
-        if (uri != null) {
-            Toast.makeText(this, " inserted item in database table inventory", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(this, " something went wrong", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void readdata() {
-
-        // 3) set a cursor to null
-        Cursor myCursor = null;
-
-        try {
-              myCursor = this.getContentResolver().query(InventoryContract.InventoryEntry.CONTENT_URI,
-                    null,
-                    null,
-                    null,
-                    BaseColumns._ID);
-
-            if (myCursor != null) {
-                myCursor.moveToFirst();
-                String ProductName = myCursor.getString(myCursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME));
-                double price = myCursor.getDouble(myCursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE));
-                int quantity = myCursor.getInt(myCursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY));
-                String suppliername = myCursor.getString(myCursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_NAME));
-                String suppliernumber = myCursor.getString(myCursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER));
-
-                Toast.makeText(this, " ProductName = " + ProductName + "Price = " + price + "quantity = " + quantity + "suppliername = " + suppliername + "suppliernumber = " + suppliernumber, Toast.LENGTH_LONG).show();
-
+                getLoaderManager().initLoader(EXISTING_INVENTORY_LOADER, null, this);
             }
 
 
-        } catch (Exception e) {
-            Log.d("activity", "catch");
-        } finally {
+            public void insertdata() {
+                ContentValues values = new ContentValues();
+                values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME, "Games");
+                values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE, 50);
+                values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_NAME, "PetsSmart");
+                values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER, "205-605-4040");
+                Uri uri = getContentResolver().insert(InventoryContract.InventoryEntry.CONTENT_URI, values);
 
-            // 8) close your cursor
-            myCursor.close();
+
+            }
+
+            private void deleteAllInventory() {
+                getContentResolver().delete(InventoryContract.InventoryEntry.CONTENT_URI, null, null);
+            }
+
+
+
+
+            @Override
+            public boolean onCreateOptionsMenu (Menu menu){
+                getMenuInflater().inflate(R.menu.menu_catalog, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onOptionsItemSelected (MenuItem item){
+
+                switch (item.getItemId()) {
+
+                    case R.id.action_insert_dummy_data:
+                        insertdata();
+                        return true;
+
+                    case R.id.action_delete_all_entries:
+                        deleteAllInventory();
+                        return true;
+                }
+                return super.onOptionsItemSelected(item);
+            }
+
+
+
+            @Override
+            public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
+
+                // Here a projection is defined. The projection specifies which columns
+                // from the database you will use after this query.
+                String[] projection = {
+                        InventoryContract.InventoryEntry._ID,
+                        InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME,
+                        InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_NAME,
+                        InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER,
+                        InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY,
+                        InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE};
+
+
+                return new CursorLoader(this,   // Parent activity context
+                        InventoryContract.InventoryEntry.CONTENT_URI,   // Provider content URI to query
+                        projection,             // Columns to include in the resulting Cursor
+                        null,                   // No selection clause
+                        null,                   // No selection arguments
+                        null);                  // Default sort order
+            }
+
+            @Override
+            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+                mCursorAdapter.swapCursor(data);
+
+            }
+
+            @Override
+            public void onLoaderReset(Loader<Cursor> loader) {
+                mCursorAdapter.swapCursor(null);
+
+            }
+
+            public void decreaseCount (int columnId, int quantity) {
+
+
+                if (quantity < 1) {
+                    Toast.makeText(this, getString(R.string.inventory_none_left_toast_msg),
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    quantity = quantity - 1;
+                    Toast.makeText(this, getString(R.string.inventory_one_deleted_toast_msg),
+                            Toast.LENGTH_SHORT).show();
+
+                    ContentValues values = new ContentValues();
+                    values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY, quantity);
+
+                    Uri updateUri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, columnId);
+
+                    getContentResolver().update(updateUri, values, null, null);
+
+                }
+            }
+
         }
-    }
-}
+
